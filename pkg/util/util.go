149949d919e8c90af6d1cf1ebfbf62498c4eed95
Sarah Vessels <cheshire137@gmail.com>
2023-12-17 14:13:43 -0600

    Add LogRequest

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -1,6 +1,9 @@
 package util
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func LogInfo(format string, a ...interface{}) {
 	fmt.Printf("ℹ️ "+format+"\n", a...)
@@ -9,6 +12,15 @@ func LogInfo(format string, a ...interface{}) {
 func LogError(a ...interface{}) {
 	fmt.Print("❌ ")
 	fmt.Println(a...)
+}
+
+func LogRequest(r *http.Request) {
+	queryStr := r.URL.RawQuery
+	if queryStr == "" {
+		LogInfo("%s %s", r.Method, r.URL.Path)
+	} else {
+		LogInfo("%s %s?%s", r.Method, r.URL.Path, r.URL.RawQuery)
+	}
 }
 
 func LogSuccess(format string, a ...interface{}) {
