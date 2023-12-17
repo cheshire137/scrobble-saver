46db9a90c14c27e6509fee27b0ad4237f5806309
Sarah Vessels <cheshire137@gmail.com>
2023-12-17 15:23:06 -0600

    Persist Last.fm user on login

--- pkg/data_store/lastfm_users.go
+++ pkg/data_store/lastfm_users.go
@@ -1,5 +1,31 @@
 package data_store
 
+import "github.com/cheshire137/lastly-likes/pkg/util"
+
+type LastfmUser struct {
+	Name       string
+	SessionKey string
+}
+
+func (ds *DataStore) UpsertLastfmUser(lastfmUser *LastfmUser) error {
+	query := `INSERT INTO lastfm_users (name, session_key) VALUES (?, ?)
+		ON CONFLICT (name) DO UPDATE SET session_key = excluded.session_key`
+	stmt, err := ds.db.Prepare(query)
+	if err != nil {
+		return err
+	}
+	encodedSessionKey, err := util.Encrypt(lastfmUser.SessionKey, ds.secret)
+	util.LogInfo("%s => %s", lastfmUser.SessionKey, encodedSessionKey)
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec(lastfmUser.Name, encodedSessionKey)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func (ds *DataStore) createLastfmUsersTable() error {
 	query := `CREATE TABLE IF NOT EXISTS lastfm_users (
 		name TEXT PRIMARY KEY,
--- pkg/server/lastfm_auth_handler.go
+++ pkg/server/lastfm_auth_handler.go
@@ -1,11 +1,17 @@
 package server
 
 import (
+	"encoding/json"
 	"net/http"
 
+	"github.com/cheshire137/lastly-likes/pkg/data_store"
 	"github.com/cheshire137/lastly-likes/pkg/lastfm"
 	"github.com/cheshire137/lastly-likes/pkg/util"
 )
+
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
 
 func (e *Env) LastfmAuthHandler(w http.ResponseWriter, r *http.Request) {
 	e.enableCors(&w)
@@ -15,4 +21,24 @@ func (e *Env) LastfmAuthHandler(w http.ResponseWriter, r *http.Request) {
 	api := lastfm.NewApi(e.config)
 	sessionResp := api.GetSession(token)
 
+	w.Header().Set("Content-Type", "application/json")
+	if sessionResp == nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(ErrorResponse{Error: "Failed to sign in with Last.fm"})
+		return
+	}
+
+	lastfmUser := data_store.LastfmUser{
+		Name:       sessionResp.Session.Name,
+		SessionKey: sessionResp.Session.Key,
+	}
+	err := e.ds.UpsertLastfmUser(&lastfmUser)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		message := "Failed to create your account: " + err.Error()
+		json.NewEncoder(w).Encode(ErrorResponse{Error: message})
+		return
+	}
+
+	json.NewEncoder(w).Encode(lastfmUser)
 }
