8aa59c5cfcb716f962db4c56d6e1d6ecafa10132
Sarah Vessels <cheshire137@gmail.com>
2023-12-17 16:08:26 -0600

    Linkify home page, show title

--- ui/src/components/AppLayout.tsx
+++ ui/src/components/AppLayout.tsx
@@ -1,14 +1,21 @@
+import { useContext } from 'react'
 import { Header, Heading, PageLayout } from '@primer/react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useHref } from 'react-router-dom'
+import { PageContext } from '../contexts/PageContext'
 
 const AppLayout = () => {
+  const { pageTitle } = useContext(PageContext)
   return <PageLayout>
     <PageLayout.Header>
       <Header>
         <Header.Item full>
           <Heading as="h1">
-            Lastly Likes
+            <Header.Link href={useHref('/')}>Lastly Likes</Header.Link>
           </Heading>
+          {pageTitle && pageTitle.length > 0 && <Heading
+            as="h2"
+            sx={{ fontWeight: 'normal', fontSize: 3, mx: 4 }}
+          >{pageTitle}</Heading>}
         </Header.Item>
       </Header>
     </PageLayout.Header>
