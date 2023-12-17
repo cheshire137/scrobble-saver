3010e3eadae3af5a23d4679a69bdcd1a5b87d306
Sarah Vessels <cheshire137@gmail.com>
2023-12-17 15:55:16 -0600

    Add page header

--- ui/src/components/AppLayout.tsx
+++ ui/src/components/AppLayout.tsx
@@ -1,8 +1,17 @@
-import { PageLayout } from '@primer/react'
+import { Header, Heading, PageLayout } from '@primer/react'
 import { Outlet } from 'react-router-dom'
 
 const AppLayout = () => {
   return <PageLayout>
+    <PageLayout.Header>
+      <Header>
+        <Header.Item full>
+          <Heading as="h1">
+            Lastly Likes
+          </Heading>
+        </Header.Item>
+      </Header>
+    </PageLayout.Header>
     <PageLayout.Content sx={{ fontSize: 2 }}>
       <Outlet />
     </PageLayout.Content>
