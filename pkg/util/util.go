d84a17608cf54ca2c7965b109321a70469132c38
Sarah Vessels <cheshire137@gmail.com>
2023-12-17 15:20:22 -0600

    Add some encrypt/decrypt utilities
    https://gist.github.com/manishtpatel/8222606

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -1,7 +1,12 @@
 package util
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -25,4 +30,50 @@ func LogRequest(r *http.Request) {
 
 func LogSuccess(format string, a ...interface{}) {
 	fmt.Printf("✅ "+format+"\n", a...)
+}
+
+func Encode(input []byte) string {
+	return base64.StdEncoding.EncodeToString(input)
+}
+
+func Decode(input string) ([]byte, error) {
+	return base64.StdEncoding.DecodeString(input)
+}
+
+func Encrypt(input string, secret string) (string, error) {
+	plaintext := []byte(input)
+	block, err := aes.NewCipher([]byte(secret))
+	if err != nil {
+		return "", err
+	}
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	// The IV needs to be unique, but not secure. It's common to include it at the beginning of the ciphertext.
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return "", err
+	}
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+	return Encode(ciphertext), nil
+}
+
+func Decrypt(input string, secret string) (string, error) {
+	ciphertext, err := Decode(input)
+	if err != nil {
+		return "", err
+	}
+	block, err := aes.NewCipher([]byte(secret))
+	if err != nil {
+		return "", err
+	}
+	if len(ciphertext) < aes.BlockSize {
+		return "", fmt.Errorf("ciphertext too short")
+	}
+	// The IV needs to be unique, but not secure. It's common to include it at the beginning of the ciphertext.
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+	stream := cipher.NewCFBDecrypter(block, iv)
+	// XORKeyStream can work in-place if the two arguments are the same.
+	stream.XORKeyStream(ciphertext, ciphertext)
+	return string(ciphertext), nil
 }
