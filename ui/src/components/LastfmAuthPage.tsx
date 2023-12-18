5ed229e837bd05190e4017b3757944a4980a7f04
Sarah Vessels <cheshire137@gmail.com>
2023-12-18 11:29:59 -0600

    Add top track period selector

--- ui/src/components/LastfmAuthPage.tsx
+++ ui/src/components/LastfmAuthPage.tsx
@@ -1,16 +1,36 @@
-import { useContext, useEffect } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { useLoaderData } from 'react-router-dom'
+import { ActionList, ActionMenu } from '@primer/react'
 import { PageContext } from '../contexts/PageContext'
+
+const topTrackPeriods = ['overall', '7day', '1month', '3month', '6month', '12month']
 
 const LastfmAuthPage = () => {
   const username = useLoaderData() as string
   const { setPageTitle } = useContext(PageContext)
+  const [period, setPeriod] = useState(topTrackPeriods[0])
 
   useEffect(() => {
     setPageTitle(`Last.fm ${username}`)
   }, [setPageTitle, username])
 
   return <div>
+    <ActionMenu>
+      <ActionMenu.Button>
+        Period: {period}
+      </ActionMenu.Button>
+      <ActionMenu.Overlay width="small">
+        <ActionList selectionVariant="single">
+          {topTrackPeriods.map(otherPeriod =>
+            <ActionList.Item
+              selected={period === otherPeriod}
+              key={otherPeriod}
+              onClick={() => setPeriod(otherPeriod)}
+            >{otherPeriod}</ActionList.Item>
+          )}
+        </ActionList>
+      </ActionMenu.Overlay>
+    </ActionMenu>
     Signed in as {username}
   </div>
 }
