790f367d0e05a8df89226e1d75cceb7ee010f3bb
Sarah Vessels <cheshire137@gmail.com>
2023-12-18 15:10:54 -0600

    Show track image

--- ui/src/components/LastfmTrackDisplay.tsx
+++ ui/src/components/LastfmTrackDisplay.tsx
@@ -1,19 +1,21 @@
 import LastfmTrack from '../models/LastfmTrack'
-import { Heading, Link } from '@primer/react'
+import { Avatar, Box, Heading, Link } from '@primer/react'
 
 interface Props {
   track: LastfmTrack
 }
 
 const LastfmTrackDisplay = ({ track }: Props) => {
-  return <div>
-    <Heading as="h3">
+  const image = track.images.find(image => image.size === 'medium')
+  return <Box sx={{ display: 'flex', alignItems: 'center', mb: 3 }}>
+    <Heading as="h3" sx={{ fontSize: 2 }}>
+      {image && <Avatar src={image.url} size={32} sx={{ mr: 2 }} />}
       <Link href={track.url} target="_blank">#{track.rank} {track.name}</Link>
     </Heading>
-    <p>
+    <Box sx={{ ml: 1 }}>
       by <Link href={track.artist.url} target="_blank">{track.artist.name}</Link>
-    </p>
-  </div>
+    </Box>
+  </Box>
 }
 
 export default LastfmTrackDisplay
