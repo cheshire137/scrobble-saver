f602c0d6cd4e2101a5d4ce8715ab2bf14611185c
Sarah Vessels <cheshire137@gmail.com>
2023-12-18 15:33:27 -0600

    Show track play count

--- ui/src/components/LastfmTrackDisplay.tsx
+++ ui/src/components/LastfmTrackDisplay.tsx
@@ -7,16 +7,20 @@ interface Props {
 
 const LastfmTrackDisplay = ({ track }: Props) => {
   const image = track.images.find(image => image.size === 'medium')
-  return <Box as="li">
-    <Box sx={{ display: 'flex', alignItems: 'center', mb: 3 }}>
-      <Heading as="h3" sx={{ fontSize: 2, display: 'flex', alignItems: 'center' }}>
-        {image && <Avatar src={image.url} size={32} sx={{ mr: 2 }} />}
-        <CounterLabel sx={{ mr: 1 }}>#{track.rank}</CounterLabel>
-        <Link href={track.url} target="_blank">{track.name}</Link>
-      </Heading>
-      <Box sx={{ ml: 1, color: 'fg.muted' }}>
-        by <Link href={track.artist.url} muted target="_blank">{track.artist.name}</Link>
-      </Box>
+  return <Box as="li" sx={{ mb: 3, display: 'flex', alignItems: 'center' }}>
+    <Heading as="h3" sx={{ fontSize: 2, display: 'flex', alignItems: 'center' }}>
+      {image && <Avatar src={image.url} size={32} sx={{ mr: 2 }} />}
+      <CounterLabel sx={{ mr: 1 }}>#{track.rank}</CounterLabel>
+      <Link href={track.url} target="_blank">{track.name}</Link>
+    </Heading>
+    <Box sx={{ ml: 1, color: 'fg.muted' }}>
+      by <Link href={track.artist.url} muted target="_blank">{track.artist.name}</Link>
+    </Box>
+    <Box sx={{ ml: 1, color: 'fg.muted' }}>
+      &middot;
+    </Box>
+    <Box sx={{ fontStyle: 'italic', ml: 1, color: 'fg.muted' }}>
+      {track.playCount} play{track.playCount === 1 ? '' : 's'}
     </Box>
   </Box>
 }
