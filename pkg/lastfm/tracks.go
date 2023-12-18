36ed04ecef71581d6e7177b462fc208421a6ba4e
Sarah Vessels <cheshire137@gmail.com>
2023-12-18 11:23:46 -0600

    Add support for other track params

--- pkg/lastfm/tracks.go
+++ pkg/lastfm/tracks.go
@@ -3,6 +3,7 @@ package lastfm
 import (
 	"encoding/xml"
 	"net/url"
+	"strconv"
 
 	"github.com/cheshire137/lastly-likes/pkg/util"
 )
@@ -23,10 +24,22 @@ type GetTopTracksResponse struct {
 }
 
 // https://www.last.fm/api/show/user.getTopTracks
-func (a *Api) GetTopTracks(user string) (*GetTopTracksResponse, error) {
+func (a *Api) GetTopTracks(user string, period string, limit int, page int) (*GetTopTracksResponse, error) {
+	if period == "" {
+		period = "3month"
+	}
+	if limit < 1 {
+		limit = 50
+	}
+	if page < 1 {
+		page = 1
+	}
 	method := "user.getTopTracks"
 	params := url.Values{}
 	params.Add("user", user)
+	params.Add("period", period)
+	params.Add("limit", strconv.Itoa(limit))
+	params.Add("page", strconv.Itoa(page))
 	var response GetTopTracksResponse
 	err := a.get(method, params, false, &response)
 	if err != nil {
--- pkg/server/lastfm_top_tracks_handler.go
+++ pkg/server/lastfm_top_tracks_handler.go
@@ -3,6 +3,7 @@ package server
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"github.com/cheshire137/lastly-likes/pkg/lastfm"
 	"github.com/cheshire137/lastly-likes/pkg/util"
@@ -12,8 +13,19 @@ func (e *Env) LastfmTopTracksHandler(w http.ResponseWriter, r *http.Request) {
 	e.enableCors(&w)
 	util.LogRequest(r)
 	user := r.URL.Query().Get("user")
+	period := r.URL.Query().Get("period")
+	limitStr := r.URL.Query().Get("limit")
+	limit, err := strconv.Atoi(limitStr)
+	if err != nil {
+		limit = 50
+	}
+	pageStr := r.URL.Query().Get("page")
+	page, err := strconv.Atoi(pageStr)
+	if err != nil {
+		page = 1
+	}
 	api := lastfm.NewApi(e.config)
-	topTracksResp, err := api.GetTopTracks(user)
+	topTracksResp, err := api.GetTopTracks(user, period, limit, page)
 
 	if err != nil {
 		w.Header().Set("Content-Type", "application/json")
