e46945d2cb2a10dffd37a37f14f63a958e4b3835
Sarah Vessels <cheshire137@gmail.com>
2023-12-18 15:43:05 -0600

    Clear page title on login page

--- ui/src/components/LastfmLoginPage.tsx
+++ ui/src/components/LastfmLoginPage.tsx
@@ -1,7 +1,15 @@
 import { Link } from '@primer/react'
+import { useEffect, useContext } from 'react'
+import { PageContext } from '../contexts/PageContext'
 
 const LastfmLoginPage = () => {
   const env = import.meta.env
+  const { setPageTitle } = useContext(PageContext)
+
+  useEffect(() => {
+    setPageTitle('')
+  }, [setPageTitle])
+
   return <p>
     <Link href={`http://www.last.fm/api/auth/?api_key=${env.VITE_LASTFM_API_KEY}`}>Log in with Last.fm</Link>
   </p>
