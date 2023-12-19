46a71e918a362c04208d78496eb92b6a8e28a4e2
Sarah Vessels <cheshire137@gmail.com>
2023-12-19 14:52:13 -0600

    Save Spotify user details

--- pkg/server/spotify_auth_handler.go
+++ pkg/server/spotify_auth_handler.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/cheshire137/lastly-likes/pkg/data_store"
 	"github.com/cheshire137/lastly-likes/pkg/spotify"
 	"github.com/cheshire137/lastly-likes/pkg/util"
 )
@@ -34,6 +35,33 @@ func (e *Env) SpotifyAuthHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(tokenResp)
+	api = spotify.NewAuthenticatedApi(e.config, e.ds, tokenResp.AccessToken)
+	userResp, err := api.GetCurrentUser()
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		message := "Failed to fetch account details from Spotify: " + err.Error()
+		json.NewEncoder(w).Encode(ErrorResponse{Error: message})
+		return
+	}
+	util.LogInfo("Spotify user", userResp)
+
+	spotifyUser := data_store.SpotifyUser{
+		Id:           userResp.Id,
+		AccessToken:  tokenResp.AccessToken,
+		RefreshToken: tokenResp.RefreshToken,
+		Scopes:       tokenResp.Scope,
+		ExpiresIn:    tokenResp.ExpiresIn,
+	}
+	err = e.ds.UpsertSpotifyUser(&spotifyUser)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		message := "Failed to save Spotify details: " + err.Error()
+		json.NewEncoder(w).Encode(ErrorResponse{Error: message})
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("http://localhost:%d#/spotify/%s", e.config.FrontendPort, spotifyUser.Id),
+		http.StatusSeeOther)
 }
