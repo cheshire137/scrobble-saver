5d2163d41326fc658919f858830a80f594a8cda3
Sarah Vessels <cheshire137@gmail.com>
2023-12-19 14:35:31 -0600

    Get Last.fm username from local storage

--- ui/src/contexts/LastfmUserContext.tsx
+++ ui/src/contexts/LastfmUserContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, PropsWithChildren, useMemo, useState } from 'react'
+import LocalStorage, { lastfmUsernameKey } from '../models/LocalStorage'
 
 export type LastfmUserContextProps = {
   user: string
@@ -8,7 +9,7 @@ export type LastfmUserContextProps = {
 export const LastfmUserContext = createContext<LastfmUserContextProps>({ user: '', setUser: () => {} })
 
 export const LastfmUserContextProvider = ({ children }: PropsWithChildren) => {
-  const [user, setUser] = useState('')
+  const [user, setUser] = useState(LocalStorage.get(lastfmUsernameKey) ?? '')
   const contextProps = useMemo(() => ({ user, setUser } satisfies LastfmUserContextProps), [user])
   return <LastfmUserContext.Provider value={contextProps}>{children}</LastfmUserContext.Provider>
 }
