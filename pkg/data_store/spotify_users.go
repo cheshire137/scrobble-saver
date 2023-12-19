c3814fafe04c1519728d3390b2a27d78c6663ff1
Sarah Vessels <cheshire137@gmail.com>
2023-12-19 14:01:08 -0600

    Add UpsertSpotifyUser

--- pkg/data_store/spotify_users.go
+++ pkg/data_store/spotify_users.go
@@ -1,11 +1,38 @@
 package data_store
 
+import "github.com/cheshire137/lastly-likes/pkg/util"
+
 type SpotifyUser struct {
 	Email        string
 	AccessToken  string
 	RefreshToken string
 	Scopes       string
 	ExpiresIn    int
+}
+
+func (ds *DataStore) UpsertSpotifyUser(spotifyUser *SpotifyUser) error {
+	query := `INSERT INTO spotify_users (email, access_token, refresh_token, scopes, expires_in)
+		VALUES (?, ?, ?, ?, ?)
+		ON CONFLICT (email) DO UPDATE SET access_token = excluded.access_token, refresh_token = excluded.refresh_token,
+		scopes = excluded.scopes, expires_in = excluded.expires_in`
+	stmt, err := ds.db.Prepare(query)
+	if err != nil {
+		return err
+	}
+	encryptedAccessToken, err := util.Encrypt(spotifyUser.AccessToken, ds.secret)
+	if err != nil {
+		return err
+	}
+	encryptedRefreshToken, err := util.Encrypt(spotifyUser.RefreshToken, ds.secret)
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec(spotifyUser.Email, encryptedAccessToken, encryptedRefreshToken, spotifyUser.Scopes,
+		spotifyUser.ExpiresIn)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (ds *DataStore) createSpotifyUsersTable() error {
