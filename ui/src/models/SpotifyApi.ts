d87ea3be73499f526e84a13930c24e70e7676cd9
Sarah Vessels <cheshire137@gmail.com>
2023-12-20 15:28:32 -0600

    Don't require album param for frontend API

--- ui/src/models/SpotifyApi.ts
+++ ui/src/models/SpotifyApi.ts
@@ -1,11 +1,11 @@
 import Api from './Api'
 
 class SpotifyApi {
-  public static async searchTracks(album: string, artist: string, track: string, limit?: number, offset?: number) {
+  public static async searchTracks(artist: string, track: string, album?: string, limit?: number, offset?: number) {
     const params = new URLSearchParams()
-    params.append('album', album)
     params.append('artist', artist)
     params.append('track', track)
+    if (album && album.trim().length > 0) params.append('album', album.trim())
     if (limit) params.append('limit', limit.toString())
     if (offset) params.append('offset', offset.toString())
     const result = await Api.get(`/spotify/search-tracks?${params.toString()}`)
