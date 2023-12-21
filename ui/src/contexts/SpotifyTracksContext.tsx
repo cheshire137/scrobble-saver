7e2e8ff11bb2b5bdb5087af422cf6ccf48fd5830
Sarah Vessels <cheshire137@gmail.com>
2023-12-21 12:27:03 -0600

    Allow adding many new tracks at once

--- ui/src/contexts/SpotifyTracksContext.tsx
+++ ui/src/contexts/SpotifyTracksContext.tsx
@@ -3,25 +3,27 @@ import SpotifyTrack from '../models/SpotifyTrack'
 
 export type SpotifyTracksContextProps = {
   tracks: SpotifyTrack[]
-  addTrack(track: SpotifyTrack): void
+  addTracks(tracks: SpotifyTrack[]): void
 }
 
 export const SpotifyTracksContext = createContext<SpotifyTracksContextProps>({
   tracks: [],
-  addTrack: () => {},
+  addTracks: () => {},
 })
 
 export const SpotifyTracksContextProvider = ({ children }: PropsWithChildren) => {
   const [tracks, setTracks] = useState<SpotifyTrack[]>([])
-  const addTrack = useCallback((track: SpotifyTrack) => {
-    setTracks(tracks => {
-      if (tracks.some(({ id }) => id === track.id)) {
-        return tracks
-      }
-      return [...tracks, track]
+  const addTracks = useCallback((newTracks: SpotifyTrack[]) => {
+    setTracks(t => {
+      const newTrackIds = newTracks.map(newTrack => newTrack.id)
+      const existingTracks = t.filter(existingTrack => !newTrackIds.includes(existingTrack.id))
+      return [...existingTracks, ...newTracks]
     })
   }, [setTracks])
-  const contextProps = useMemo(() => ({ tracks, addTrack, } satisfies SpotifyTracksContextProps), [tracks, addTrack])
+  const contextProps = useMemo(() => ({
+    tracks,
+    addTracks,
+  } satisfies SpotifyTracksContextProps), [tracks, addTracks])
 
   return <SpotifyTracksContext.Provider value={contextProps}>{children}</SpotifyTracksContext.Provider>
 }
