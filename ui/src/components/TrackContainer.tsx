cf28091a60807762402d658a65b0485c8baf8b05
Sarah Vessels <cheshire137@gmail.com>
2023-12-22 14:26:26 -0600

    Fix sx props not coming through track container

--- ui/src/components/TrackContainer.tsx
+++ ui/src/components/TrackContainer.tsx
@@ -1,22 +1,18 @@
 import styled from 'styled-components'
 import { ActionList, Box } from '@primer/react'
-import deepmerge from 'deepmerge'
 
-const styleAttrs = {
-  mb: 3,
-  borderBottom: '1px solid',
-  borderColor: 'border.default',
-  pb: 3,
-}
-
-export const TrackContainerBox = styled(Box).attrs(deepmerge(styleAttrs, {
+export const TrackContainerBox = styled(Box).attrs({
   as: 'li',
-}))`
-  height: 61px;
-`
-
-export const TrackContainerActionListItem = styled(ActionList.Item).attrs({
-  sx: styleAttrs,
 })`
   height: 61px;
+  border-bottom: 1px solid ${props => props.theme.colors.border.default};
+  margin-bottom: ${props => props.theme.space[3]};
+  padding-bottom: ${props => props.theme.space[3]};
+`
+
+export const TrackContainerActionListItem = styled(ActionList.Item)`
+  height: 61px;
+  border-bottom: 1px solid ${props => props.theme.colors.border.default};
+  margin-bottom: ${props => props.theme.space[3]};
+  padding-bottom: ${props => props.theme.space[3]};
 `
