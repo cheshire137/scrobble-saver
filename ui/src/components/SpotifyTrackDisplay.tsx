10fd17165267642dfbc12ce0a947cca05fdfa318
Sarah Vessels <cheshire137@gmail.com>
2023-12-22 15:08:25 -0600

    Show green heart for saved tracks

--- ui/src/components/SpotifyTrackDisplay.tsx
+++ ui/src/components/SpotifyTrackDisplay.tsx
@@ -1,7 +1,10 @@
-import { Avatar, Box, Link } from '@primer/react'
+import { useContext } from 'react'
+import { Avatar, Box, Link, Octicon } from '@primer/react'
+import { HeartFillIcon } from '@primer/octicons-react'
 import SpotifyTrack from '../models/SpotifyTrack'
 import SpotifyAlbumDisplay from './SpotifyAlbumDisplay'
 import { TrackContainerActionListItem } from './TrackContainer'
+import { SpotifySavedTracksContext } from '../contexts/SpotifySavedTracksContext'
 
 interface Props {
   track: SpotifyTrack
@@ -9,6 +12,9 @@ interface Props {
 
 const SpotifyTrackDisplay = ({ track }: Props) => {
   const image = track.smallImage()
+  const { savedTrackIds } = useContext(SpotifySavedTracksContext)
+  const isTrackSavedToSpotifyLibrary = savedTrackIds.has(track.id)
+
   return <TrackContainerActionListItem>
     <Link sx={{ display: 'flex', alignItems: 'center' }} href={track.url} target="_blank">
       {image && <Avatar src={image.url} square alt={`${track.album.name} cover art`} size={32} sx={{ mr: 2 }} />}
@@ -17,6 +23,7 @@ const SpotifyTrackDisplay = ({ track }: Props) => {
         <SpotifyAlbumDisplay album={track.album} />
       </Box>
     </Link>
+    {isTrackSavedToSpotifyLibrary && <Octicon icon={HeartFillIcon} sx={{ ml: 2, color: 'spotify.fg' }} />}
   </TrackContainerActionListItem>
 }
 
