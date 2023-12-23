27f61bce2fb37e2d6ae5877d6a39edc9de3d9482
Sarah Vessels <cheshire137@gmail.com>
2023-12-23 14:38:38 -0600

    Absorb SpotifyTrackMetadata

--- ui/src/components/SpotifyTrackDisplay.tsx
+++ ui/src/components/SpotifyTrackDisplay.tsx
@@ -1,11 +1,11 @@
 import { useContext } from 'react'
-import { ActionList } from '@primer/react'
+import { ActionList, Avatar, Box } from '@primer/react'
 import SpotifyTrack from '../models/SpotifyTrack'
 import { TrackContainerActionListItem } from './TrackContainer'
 import SpotifySavedTrackStatus from './SpotifySavedTrackStatus'
-import SpotifyTrackMetadata from './SpotifyTrackMetadata'
 import { SpotifySelectedTracksContext } from '../contexts/SpotifySelectedTracksContext'
 import { SpotifySavedTracksContext } from '../contexts/SpotifySavedTracksContext'
+import SpotifyAlbumDisplay from './SpotifyAlbumDisplay'
 
 interface Props {
   track: SpotifyTrack
@@ -18,6 +18,7 @@ const SpotifyTrackDisplay = ({ track, selectable, ...props }: Props) => {
   const { savedTrackIds } = useContext(SpotifySavedTracksContext)
   const isTrackSaved = savedTrackIds.has(track.id)
   const isTrackSelected = selectedTrackIds.has(track.id)
+  const image = track.smallImage()
 
   return <TrackContainerActionListItem
     disabled={isTrackSaved && selectable}
@@ -30,7 +31,13 @@ const SpotifyTrackDisplay = ({ track, selectable, ...props }: Props) => {
       color: isTrackSaved ? 'fg.default' : undefined,
     }}
   >
-    <SpotifyTrackMetadata track={track} />
+    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      {image && <Avatar src={image.url} square alt={`${track.album.name} cover art`} size={32} sx={{ mr: 2 }} />}
+      <Box>
+        {track.name} by {track.artists.map(artist => artist.name).join(', ')}
+        <SpotifyAlbumDisplay album={track.album} />
+      </Box>
+    </Box>
     <ActionList.TrailingVisual sx={{ height: 'auto', display: 'flex', alignItems: 'center' }}>
       <SpotifySavedTrackStatus track={track} />
     </ActionList.TrailingVisual>
