8869186b309d84b6091963851a6281eb14f21301
Sarah Vessels <cheshire137@gmail.com>
2023-12-23 12:37:08 -0600

    Add addSavedTrackIds to context

--- ui/src/contexts/SpotifySavedTracksContext.tsx
+++ ui/src/contexts/SpotifySavedTracksContext.tsx
@@ -3,6 +3,7 @@ import { createContext, PropsWithChildren, useCallback, useMemo, useState } from
 export type SpotifySavedTracksContextProps = {
   savedStatusByTrackId: Map<string, boolean>
   addTrackStatuses(updates: Map<string, boolean>): void
+  addSavedTrackIds(ids: string[]): void
   notSavedTrackIds: Set<string>
   savedTrackIds: Set<string>
 }
@@ -10,6 +11,7 @@ export type SpotifySavedTracksContextProps = {
 export const SpotifySavedTracksContext = createContext<SpotifySavedTracksContextProps>({
   savedStatusByTrackId: new Map(),
   addTrackStatuses: () => {},
+  addSavedTrackIds: () => {},
   notSavedTrackIds: new Set(),
   savedTrackIds: new Set(),
 })
@@ -19,15 +21,23 @@ export const SpotifySavedTracksContextProvider = ({ children }: PropsWithChildre
   const addTrackStatuses = useCallback((updates: Map<string, boolean>) => {
     setSavedStatusByTrackId(prev => new Map([...prev, ...updates]))
   }, [setSavedStatusByTrackId])
+  const addSavedTrackIds = useCallback((trackIds: string[]) => {
+    setSavedStatusByTrackId(prev => {
+      const result = new Map([...prev])
+      trackIds.forEach(id => result.set(id, true))
+      return result
+    })
+  }, [setSavedStatusByTrackId])
   const contextProps = useMemo(() => {
     const trackIds = Array.from(savedStatusByTrackId.keys())
     return {
       savedStatusByTrackId,
       addTrackStatuses,
+      addSavedTrackIds,
       notSavedTrackIds: new Set(trackIds.filter(trackId => !savedStatusByTrackId.get(trackId))),
       savedTrackIds: new Set(trackIds.filter(trackId => savedStatusByTrackId.get(trackId))),
     } satisfies SpotifySavedTracksContextProps
-  }, [savedStatusByTrackId, addTrackStatuses])
+  }, [savedStatusByTrackId, addTrackStatuses, addSavedTrackIds])
 
   return <SpotifySavedTracksContext.Provider value={contextProps}>{children}</SpotifySavedTracksContext.Provider>
 }
