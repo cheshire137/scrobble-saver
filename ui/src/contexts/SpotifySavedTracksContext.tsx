1982c26105d0910455058845c7163cf19b9849ee
Sarah Vessels <cheshire137@gmail.com>
2023-12-23 12:22:25 -0600

    Add more props to SpotifySavedTracksContext

--- ui/src/contexts/SpotifySavedTracksContext.tsx
+++ ui/src/contexts/SpotifySavedTracksContext.tsx
@@ -3,11 +3,15 @@ import { createContext, PropsWithChildren, useCallback, useMemo, useState } from
 export type SpotifySavedTracksContextProps = {
   savedStatusByTrackId: Map<string, boolean>
   addTrackStatuses(updates: Map<string, boolean>): void
+  notSavedTrackIds: Set<string>
+  savedTrackIds: Set<string>
 }
 
 export const SpotifySavedTracksContext = createContext<SpotifySavedTracksContextProps>({
   savedStatusByTrackId: new Map(),
   addTrackStatuses: () => {},
+  notSavedTrackIds: new Set(),
+  savedTrackIds: new Set(),
 })
 
 export const SpotifySavedTracksContextProvider = ({ children }: PropsWithChildren) => {
@@ -15,10 +19,15 @@ export const SpotifySavedTracksContextProvider = ({ children }: PropsWithChildre
   const addTrackStatuses = useCallback((updates: Map<string, boolean>) => {
     setSavedStatusByTrackId(prev => new Map([...prev, ...updates]))
   }, [setSavedStatusByTrackId])
-  const contextProps = useMemo(() => ({
-    savedStatusByTrackId,
-    addTrackStatuses,
-  } satisfies SpotifySavedTracksContextProps), [savedStatusByTrackId, addTrackStatuses])
+  const contextProps = useMemo(() => {
+    const trackIds = Array.from(savedStatusByTrackId.keys())
+    return {
+      savedStatusByTrackId,
+      addTrackStatuses,
+      notSavedTrackIds: new Set(trackIds.filter(trackId => !savedStatusByTrackId.get(trackId))),
+      savedTrackIds: new Set(trackIds.filter(trackId => savedStatusByTrackId.get(trackId))),
+    } satisfies SpotifySavedTracksContextProps
+  }, [savedStatusByTrackId, addTrackStatuses])
 
   return <SpotifySavedTracksContext.Provider value={contextProps}>{children}</SpotifySavedTracksContext.Provider>
 }
