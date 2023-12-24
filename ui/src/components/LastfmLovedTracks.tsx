e10332913165fbb8f35ed58ba79c16860cfcea62
Sarah Vessels <cheshire137@gmail.com>
2023-12-24 14:09:22 -0600

    Fix indentation

--- ui/src/components/LastfmLovedTracks.tsx
+++ ui/src/components/LastfmLovedTracks.tsx
@@ -10,20 +10,20 @@ const LastfmLovedTracks = () => {
   if (!results) return null
 
   return <Box>
-  <Heading sx={{ mb: 2, color: 'lastfm.fg', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-    <span>
-      <Avatar
-        sx={{ mr: 2, display: 'inline-block', verticalAlign: 'middle', boxShadow: 'none' }}
-        size={32}
-        src={LastfmLogo}
-      />
-      Last.fm loved tracks
-    </span>
-  </Heading>
-  <Box as="ol" sx={{ listStyle: 'none', pl: 0 }}>
-    {results.tracks.map(track => <LastfmTrackDisplay key={track.url} track={track} />)}
+    <Heading sx={{ mb: 2, color: 'lastfm.fg', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+      <span>
+        <Avatar
+          sx={{ mr: 2, display: 'inline-block', verticalAlign: 'middle', boxShadow: 'none' }}
+          size={32}
+          src={LastfmLogo}
+        />
+        Last.fm loved tracks
+      </span>
+    </Heading>
+    <Box as="ol" sx={{ listStyle: 'none', pl: 0 }}>
+      {results.tracks.map(track => <LastfmTrackDisplay key={track.url} track={track} />)}
+    </Box>
   </Box>
-</Box>
 }
 
 export default LastfmLovedTracks
